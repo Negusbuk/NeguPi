039a9f0b0f8b627dabab28efdb88e6e2d6e29fa5
Negusbuk <Andreas Mussgiller>
2014-01-08 17:38:50 +0100

    update of copyright statement

--- src/PiFace.cc
+++ src/PiFace.cc
@@ -2,6 +2,9 @@
  **
  **  Copyright (C) 2013, 2014 Andreas Mussgiller
  **
+ **  based on libpifacedigital by Thomas Preston
+ **           http://github.com/piface/libpifacedigital
+ **
  **  This program is free software: you can redistribute it and/or modify
  **  it under the terms of the GNU General Public License as published by
  **  the Free Software Foundation, either version 3 of the License, or
